e12f627001b7e4d533e7ec27684fbb2e2fec862e
winmord <winmord@yandex.ru>
2023-06-28 19:25:49 +0300

    feature: добавить структуру сервиса статистики

---
+++ stats-service/service/src/test/java/ru/practicum/service/ServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package ru.practicum.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Do nothing because of context load test
+	}
+
+}
