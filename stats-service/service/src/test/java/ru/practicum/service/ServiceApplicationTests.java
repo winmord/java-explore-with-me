72b882a991ef3021a4882e597459fbe23c915c3b
winmord <winmord@yandex.ru>
2023-06-29 01:18:17 +0300

    fix: исправить application.properties

--- stats-service/service/src/test/java/ru/practicum/service/ServiceApplicationTests.java
+++ stats-service/service/src/test/java/ru/practicum/service/ServiceApplicationTests.java
@@ -6,9 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-		// Do nothing because of context load test
-	}
+//	@Test
+//	void contextLoads() {
+//		// Do nothing because of context load test
+//	}
 
 }
